fbfca0f4478bea6fd0310638f4360cf27fc8c3db
mysak <mysak@cloudcode.cz>
2020-11-14 18:14:05 +0100

    PROCISTENI

--- test.py
+++ test.py
@@ -1,28 +1,4 @@
-import os
-import numpy as np
-q = np.zeros(20)
-q = q*8
-a = 8
-a = a*8
-os.listdir('.')
-print('hi')
 
-def alesovafunkce():
-    print('haha')
-    pass
-alesovafunkce()
-
-def funkce3():
-    #hahaha
-    pass
-#merge
-#den4
-#den5
-
-
-#dalsidulezitavec
-
-# jo vypadá to ok
 
 
 
